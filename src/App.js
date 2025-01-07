15426e79682fdf08a63913105a8560f665adf3cb
WestTand <22649051.phat@student.iuh.edu.vn>
2025-01-07 19:12:36 +0700

    Test1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Học tập cùng Linana ngu ngốc
         </p>
         <a
           className="App-link"
